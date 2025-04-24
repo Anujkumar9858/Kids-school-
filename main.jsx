80d3e83314c17951ad2b59800bf5448a2d5c95c9
Anuj kumar <148762270+Anujkumar9858@users.noreply.github.com>
2025-04-24 22:51:29 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'; 
+// import './index.css';
 import { BrowserRouter } from "react-router-dom"; 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import App from './App';
 import Appcollage from './Appcollage';
 
 const root = createRoot(document.getElementById("root")); 
